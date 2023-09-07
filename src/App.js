446600128182ea5bbd5a8ae738dd203f0ba2c91d
Toan Nguyen <80366569+toannguyen0312@users.noreply.github.com>
2023-09-06 21:57:57 -0700

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import logo from "./photos/z4508851027414_4264c88fff731b34f685f6768b92939b.jpg"
 
 function App() {
   return (
-    <h2>
-      <img style={{ width: "20%", height: "10%" }} src={logo} />
-      <p>028 3526 4168 | 0934 043 188</p>
-      
+    <h2 style={{display: 'flex', flexDirection: 'row',}}>
+      <img style={{ width: "30%", height: "10%" }} src={logo} />
+      <p>028 3526 4168 | 0934 043 188</p>      
     </h2>
   );
 }
